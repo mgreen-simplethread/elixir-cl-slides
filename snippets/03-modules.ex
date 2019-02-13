e55c3bee387fe3b23e7b889dcab2054cf8345026
Mike Green <mike.green@simplethread.com>
2019-02-13 10:02:17 -0500

    Add module doc examples, finish conditionals slide

--- snippets/03-modules.ex
+++ snippets/03-modules.ex
@@ -1,6 +1,18 @@
 defmodule Math do
+  @moduledoc """
+  This is a module containing blah blah blah
+  """
+
   @my_constant 3.14
 
+  @doc """
+  Determine if a number is zero or not
+
+  ## Examples:
+
+    iex> Math.zero?(0)
+    true
+  """
   def zero?(0) do
     true
   end
@@ -15,3 +27,13 @@ Math.zero?(1)
 
 Math.zero?(0)
 # => true
+
+defmodule Say do
+  def something(thing \\ "hello") do
+    IO.puts(format(thing))
+  end
+
+  defp format(str) do
+    String.downcase(str)
+  end
+end
--- snippets/05-conditionals.ex
+++ snippets/05-conditionals.ex
@@ -1,8 +1,16 @@
 defmodule Response do
   defstruct status: nil, body: nil, headers: nil
+
+  def success do
+    %Response{status: 200, body: "It worked", headers: %{}}
+  end
+
+  def failure(status \\ 500) do
+    %Response{status: status, body: "oops", headers: %{}}
+  end
 end
 
-resp = %Response{status: 200, body: "It worked", headers: %{whatever: "value"}}
+resp = Response.success()
 
 case resp do
   %Response{status: 500} -> "Failure!"
@@ -10,3 +18,21 @@ case resp do
   _ -> "Something unexpected"
 end
 # => "It worked"
+
+cond do
+  2 + 2 == 5 ->
+    "This will never match"
+  false ->
+    "Nor will this"
+  resp.status == 200 ->
+    "This is what will match"
+end
+# => "This is what will match"
+
+if resp.status == 200 do
+  "something"
+end
+
+unless resp.status == 500 do
+  "something broke"
+end
