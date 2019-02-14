77c71bfc3bc66446fc929acf2fc9c82620c0ae85
Mike Green <mike.green@simplethread.com>
2019-02-14 15:20:58 -0500

    Add phoenix book cover image, update data types with charlists

--- snippets/02-data-types.ex
+++ snippets/02-data-types.ex
@@ -17,10 +17,20 @@ my_list = [1, 2, 3, "whatever", <<255>>]
 Enum.count(my_list) # O(n) complexity
 # => 5
 
+my_list ++ [4, 5, 6]
+# => [1, 2, 3, "whatever", <<255>>, 4, 5, 6]
+
+char_list = 'Es ist nich ein string'
+List.first(char_list)
+# => 69
+
 my_tuple = {:ok, "some response"}
 
 keyword_list = [this: "that", them: "us"]
 # => [{:this, "that"}, {:them, "us"}]
 
-my_map = %{raisin: "heck"}
-
+my_map = %{raisin: "heck", "string_key" => [2, 3, 4]}
+my_map["string_key"]
+# => [2, 3, 4]
+my_map.raisin
+# => "heck"
